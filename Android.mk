d377475cc42759734d527cd489bf2ca6a2873306
Sahul Krishan <05.sahulkrishan@gmail.com>
2018-02-26 21:04:30 +0100

    Only check for source files in "src/"

--- Android.mk
+++ Android.mk
@@ -20,5 +20,5 @@ include $(CLEAR_VARS)
 LOCAL_PACKAGE_NAME := ANCQuickSettings
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEDGED_MODULE := true
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
